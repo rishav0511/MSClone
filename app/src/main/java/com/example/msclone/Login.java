d49482047e85beb134e9641328c6e79075161816
Rishav <rishavraj0511@gmail.com>
2021-06-25 20:53:30 +0530

    Some design

--- app/src/main/java/com/example/msclone/Login.java
+++ app/src/main/java/com/example/msclone/Login.java
@@ -2,13 +2,33 @@ package com.example.msclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginBtn;
+    private Button mCreateBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmailEditText = findViewById(R.id.mEmailEditText);
+        mPasswordEditText = findViewById(R.id.mPasswordEditText);
+        mLoginBtn = findViewById(R.id.mLoginBtn);
+        mCreateBtn = findViewById(R.id.mCreateBtn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Signup.class));
+            }
+        });
     }
 }
