59062879ec3cd38919cf497f7ad2e03aefdf8f3a
Rishav <rishavraj0511@gmail.com>
2021-06-26 23:28:34 +0530

    pics in dashboard

--- app/src/main/java/com/example/msclone/Dashboard.java
+++ app/src/main/java/com/example/msclone/Dashboard.java
@@ -1,11 +1,15 @@
 package com.example.msclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -54,6 +58,28 @@ public class Dashboard extends AppCompatActivity {
                 JitsiMeetActivity.launch(Dashboard.this,options);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                Toast.makeText(this,"search clicked",Toast.LENGTH_SHORT).show();;
+                break;
+            case R.id.groups:
+                Toast.makeText(this,"groups clicked",Toast.LENGTH_SHORT).show();;
+                break;
+            case R.id.invite:
+                Toast.makeText(this,"invite clicked",Toast.LENGTH_SHORT).show();;
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
