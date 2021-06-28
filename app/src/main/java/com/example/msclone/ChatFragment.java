3ce3bf438ffa5b415e4b1d50b020f9eec8aa5efe
Rishav <rishavraj0511@gmail.com>
2021-06-28 20:24:22 +0530

    Fixed recycler view

--- app/src/main/java/com/example/msclone/ChatFragment.java
+++ app/src/main/java/com/example/msclone/ChatFragment.java
@@ -1,14 +1,12 @@
 package com.example.msclone;
 
-import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.msclone.databinding.FragmentChatBinding;
 import com.google.firebase.database.DataSnapshot;
