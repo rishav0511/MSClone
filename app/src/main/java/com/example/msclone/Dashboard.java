7ec91fa7f2a72dda06db4d6314fb4ada7869cf12
Rishav <rishavraj0511@gmail.com>
2021-07-07 17:28:02 +0530

    logout implemented

--- app/src/main/java/com/example/msclone/Dashboard.java
+++ app/src/main/java/com/example/msclone/Dashboard.java
@@ -1,27 +1,19 @@
 package com.example.msclone;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.msclone.databinding.ActivityDashboardBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import org.jitsi.meet.sdk.JitsiMeet;
-import org.jitsi.meet.sdk.JitsiMeetActivity;
-import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Dashboard extends AppCompatActivity {
 
